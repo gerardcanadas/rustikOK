c69b19cd4378d2bcf0d01ff1df2f9708da285a92
Gerard Cañadas <gerard.canadas@sasi.com.es>
2016-06-22 09:54:39 +0200

    added errors block

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -4,5 +4,14 @@ section .text
 bits 32
 start:
 	; print 'OK' to screen
-	mov dword [0xb8000], 0x2f4b2f4f
+	mov dword [0xb8000], 0x4f4b4f4f ; print in red
+	mov dword [0xb8000], 0x2f4b2f4f ; print in green
 	hlt
+error:
+	; Prints `ERR: ` and the given error code to screen and hangs.
+	; parameter: error code (in ascii) in al
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov byte  [0xb800a], al
+    hlt
